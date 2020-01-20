ec8698061a92dda1a1c5307e1fc4947076c70a26
Pavel Usoltsev <jpc_work@mail.ru>
2020-01-20 19:11:23 +0300

    Task 1.2 done

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,17 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+
+    if(name === undefined || name === null)
+      return false;
+
+    if(name.length < 4)
+    return false;
+
+  if(name.indexOf(' ',0) > -1)
+    return false;
+
+  return true;
 }
 
 function sayHello() {
