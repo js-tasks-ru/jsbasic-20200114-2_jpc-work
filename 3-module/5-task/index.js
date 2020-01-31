ae22cc140185a04aa2ec2f7716ad0582eb34ec5f
Pavel Usoltsev <jpc_work@mail.ru>
2020-01-31 18:25:13 +0300

    Task 3.5 done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,16 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
+	if(a > b)
+	{
+		const c = a;
+		a = b;
+		b = c;
+	}
+
+	return arr.filter((elem) => {
+		if(elem >= a && elem <= b)
+			return true;
+		return false;
+	})
 }
