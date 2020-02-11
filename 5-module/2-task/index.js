246dfe3ac40dd1a2064f8c87fe00b410efadf7b9
Pavel Usoltsev <jpc_work@mail.ru>
2020-02-11 18:47:22 +0300

    task 5.2 done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -13,11 +13,11 @@
  *
  * @constructor
  */
-function SortableTable(items) {
+//function SortableTable(items) {
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
-  this.el = document.createElement('table');
+  //this.el = document.createElement('table');
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +25,56 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+//  this.sort = (column, desc = false) => {};
+//}
+
+class SortableTable {
+  constructor(items)
+  {
+    this.el = document.createElement('table');        
+    this.items = items;
+    const headerTemplate = `<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>`;
+    this.el.insertAdjacentHTML('afterbegin',headerTemplate);
+    const bodyTemplate = `<tbody></tbody>`;
+    this.el.insertAdjacentHTML('beforeend',bodyTemplate);
+    this.colHeaders = ['name', 'age', 'salary', 'city'];
+    this.render();
+  }
+
+  render(desc = false)
+  {
+    const tableBody = this.el.querySelector("tbody");
+    const place = desc ? 'afterbegin' : 'beforeend'; 
+
+    tableBody.innerHTML = "";
+    for(const item of this.items)
+    {
+      const template = `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td></tr>`;
+      tableBody.insertAdjacentHTML(place ,template);
+    }
+  }
+
+  sort(column, desc = false)
+  {
+    
+    this.sortColumn = column;
+
+    this.items.sort((a,b) => {
+        const index = this.colHeaders[this.sortColumn];
+        let result = a[index] - b[index];
+        if(!isNaN(result))
+          return result;
+
+        if(a[index] > b[index])
+          return 1;
+
+        if(a[index] == b[index])
+          return 0;
+
+        return -1;
+    });
+
+    this.render(desc);
+  }
+
 }
